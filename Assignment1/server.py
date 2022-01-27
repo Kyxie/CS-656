d6d9c189dc33cfee6937c55d0069e0b1cfe0bcca
Kyxie <kyriexie@outlook.com>
2022-01-27 15:57:05 -0500

    add README

---
+++ Assignment1/server.py
@@ -0,0 +1,23 @@
+r'''
+Date: 2022-01-27 15:29:49
+LastEditors: Kunyang Xie
+LastEditTime: 2022-01-27 15:55:28
+FilePath: \Assignment1\sever.py
+'''
+
+from socket import *
+
+
+class Server:
+    def __init__(self):
+        ...
+
+    def negotiate(self):
+        ...
+
+    def transaction(self):
+        ...
+
+
+if __name__ == '__main__':
+    ...
--- Assignment1/sever.py
+++
@@ -1,6 +0,0 @@
-r'''
-Date: 2022-01-27 15:29:49
-LastEditors: Kunyang Xie
-LastEditTime: 2022-01-27 15:29:49
-FilePath: \Assignment1\sever.py
-'''
